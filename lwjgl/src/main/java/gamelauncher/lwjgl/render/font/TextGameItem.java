13d1c1fd7b6afed77837cab4b1104a44f5267dbd
laure <laure@LAPTOP-UKDA4NSL>
2022-05-16 12:33:49 +0200

    updated

---
+++ lwjgl/src/main/java/gamelauncher/lwjgl/render/font/TextGameItem.java
@@ -0,0 +1,15 @@
+package gamelauncher.lwjgl.render.font;
+
+import gamelauncher.lwjgl.render.GameItem;
+
+public class TextGameItem extends GameItem {
+
+	public TextGameItem(String text, float size) {
+		super();
+		
+		
+		
+	}
+	
+	
+}
