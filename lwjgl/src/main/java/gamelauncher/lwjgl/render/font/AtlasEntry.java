ce3b0d7ba86ae3eecaa091cc7775d2677509cf59
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-01-09 00:57:16 +0100

    updated

--- lwjgl/src/main/java/gamelauncher/lwjgl/render/font/AtlasEntry.java
+++ lwjgl/src/main/java/gamelauncher/lwjgl/render/font/AtlasEntry.java
@@ -17,21 +17,6 @@ class AtlasEntry {
 		this.texture = texture;
 		this.entry = entry;
 		this.bounds = bounds;
-
-		NumberValue z1 = NumberValue.constant(10);
-		NumberValue z2 = NumberValue.constant(10);
-		NumberValue z3 = z1.add(z2);
-
-		z1.setValue(10);
-		z1.setValue(11);
-		z1.setValue(12);
-		z1.setValue(13);
-
-		z1.bind(z2);
-
-		z3.getNumber();
-		z3.getNumber();
-
 	}
 
 	public Rectangle getBounds() {
