7c10f35de8e7ba24cd2f82f36e23d6965383a0ba
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-04-28 10:44:50 +0200

    updated

--- base/src/main/java/gamelauncher/engine/network/packet/ByteMemory.java
+++ base/src/main/java/gamelauncher/engine/network/packet/ByteMemory.java
@@ -5,115 +5,81 @@ package gamelauncher.engine.network.packet;
  */
 public abstract class ByteMemory {
 
-	/**
-	 * @return the number of bytes this memory can contain
-	 */
-	public abstract int capacity();
+    /**
+     * @return the number of bytes this memory can contain
+     */
+    public abstract int capacity();
 
-	/**
-	 * @param index
-	 * @return the byte at the given index
-	 */
-	public abstract byte getByte(int index);
+    /**
+     * @return the byte at the given index
+     */
+    public abstract byte getByte(int index);
 
-	/**
-	 * @param index
-	 * @return the short at the given index
-	 */
-	public abstract short getShort(int index);
+    /**
+     * @return the short at the given index
+     */
+    public abstract short getShort(int index);
 
-	/**
-	 * @param index
-	 * @return the int at the given index
-	 */
-	public abstract int getInt(int index);
+    /**
+     * @return the int at the given index
+     */
+    public abstract int getInt(int index);
 
-	/**
-	 * @param index
-	 * @return the long at the given index
-	 */
-	public abstract long getLong(int index);
+    /**
+     * @return the long at the given index
+     */
+    public abstract long getLong(int index);
 
-	/**
-	 * @param index
-	 * @return the float at the given index
-	 */
-	public abstract float getFloat(int index);
+    /**
+     * @return the float at the given index
+     */
+    public abstract float getFloat(int index);
 
-	/**
-	 * @param index
-	 * @return the double at the given index
-	 */
-	public abstract double getDouble(int index);
+    /**
+     * @return the double at the given index
+     */
+    public abstract double getDouble(int index);
 
-	/**
-	 * Reads {@code length} bytes into {@code dst} with offset {@code dstOffset}
-	 * from index {@code index}
-	 * 
-	 * @param index
-	 * @param dst
-	 * @param dstOffset
-	 * @param length
-	 */
-	public abstract void getBytes(int index, byte[] dst, int dstOffset, int length);
+    /**
+     * Reads {@code length} bytes into {@code dst} with offset {@code dstOffset}
+     * from index {@code index}
+     */
+    public abstract void getBytes(int index, byte[] dst, int dstOffset, int length);
 
-	/**
-	 * Sets the byte at the given index
-	 * 
-	 * @param index
-	 * @param value
-	 */
-	public abstract void setByte(int index, byte value);
+    /**
+     * Sets the byte at the given index
+     */
+    public abstract void setByte(int index, byte value);
 
-	/**
-	 * Sets the short at the given index
-	 * 
-	 * @param index
-	 * @param value
-	 */
-	public abstract void setShort(int index, short value);
+    /**
+     * Sets the short at the given index
+     */
+    public abstract void setShort(int index, short value);
 
-	/**
-	 * Sets the int at the given index
-	 * 
-	 * @param index
-	 * @param value
-	 */
-	public abstract void setInt(int index, int value);
+    /**
+     * Sets the int at the given index
+     */
+    public abstract void setInt(int index, int value);
 
-	/**
-	 * Sets the long at the given index
-	 * 
-	 * @param index
-	 * @param value
-	 */
-	public abstract void setLong(int index, long value);
+    /**
+     * Sets the long at the given index
+     */
+    public abstract void setLong(int index, long value);
 
-	/**
-	 * Sets the float at the given index
-	 * 
-	 * @param index
-	 * @param value
-	 */
-	public abstract void setFloat(int index, float value);
+    /**
+     * Sets the float at the given index
+     */
+    public abstract void setFloat(int index, float value);
 
-	/**
-	 * Sets the double at the given index
-	 * 
-	 * @param index
-	 * @param value
-	 */
-	public abstract void setDouble(int index, double value);
+    /**
+     * Sets the double at the given index
+     */
+    public abstract void setDouble(int index, double value);
 
-	/**
-	 * Writes {@code length} bytes from {@code data} offset by {@code dataOffset} at
-	 * index {@code index}
-	 * 
-	 * @param index
-	 * @param data
-	 * @param dataOffset
-	 * @param length
-	 */
-	public abstract void setBytes(int index, byte[] data, int dataOffset, int length);
+    /**
+     * Writes {@code length} bytes from {@code data} offset by {@code dataOffset} at
+     * index {@code index}
+     */
+    public abstract void setBytes(int index, byte[] data, int dataOffset, int length);
 
 }
--- base/src/main/java/gamelauncher/engine/network/packet/PacketBuffer.java
+++ base/src/main/java/gamelauncher/engine/network/packet/PacketBuffer.java
@@ -1,298 +1,274 @@
 package gamelauncher.engine.network.packet;
 
+import de.dasbabypixel.annotations.Api;
+
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author DasBabyPixel
  */
+@Api
 public class PacketBuffer {
 
-	ByteMemory memory;
+    private ByteMemory memory;
+    private int readerIndex = 0;
+    private int writerIndex = 0;
 
-	int readerIndex = 0;
+    public PacketBuffer(ByteMemory memory) {
+        this.memory = memory;
+    }
 
-	int writerIndex = 0;
+    /**
+     * @return the reader index
+     */
+    @Api public int readerIndex() {
+        return readerIndex;
+    }
 
-	/**
-	 * @param memory
-	 */
-	public PacketBuffer(ByteMemory memory) {
-		this.memory = memory;
-	}
+    /**
+     * Sets the reader index
+     *
+     * @return the old reader index
+     */
+    @Api public int readerIndex(int readerIndex) {
+        int old = this.readerIndex;
+        this.readerIndex = readerIndex;
+        return old;
+    }
 
-	/**
-	 * @return the reader index
-	 */
-	public int readerIndex() {
-		return readerIndex;
-	}
+    /**
+     * @return the writer index
+     */
+    @Api public int writerIndex() {
+        return writerIndex;
+    }
 
-	/**
-	 * Sets the reader index
-	 * 
-	 * @param readerIndex
-	 * @return the old reader index
-	 */
-	public int readerIndex(int readerIndex) {
-		int old = this.readerIndex;
-		this.readerIndex = readerIndex;
-		return old;
-	}
+    /**
+     * Sets the writer index
+     *
+     * @return the old writer index
+     */
+    @Api public int writerIndex(int writerIndex) {
+        if (writerIndex >= this.memory.capacity()) {
+            throw new ArrayIndexOutOfBoundsException(writerIndex + " >= " + this.memory.capacity());
+        }
+        int old = this.writerIndex;
+        this.writerIndex = writerIndex;
+        return old;
+    }
 
-	/**
-	 * @return the writer index
-	 */
-	public int writerIndex() {
-		return writerIndex;
-	}
+    /**
+     * @return the number of readable bytes
+     */
+    @Api public int readableBytes() {
+        return this.writerIndex - this.readerIndex;
+    }
 
-	/**
-	 * Sets the writer index
-	 * 
-	 * @param writerIndex
-	 * @return the old writer index
-	 */
-	public int writerIndex(int writerIndex) {
-		if (writerIndex >= this.memory.capacity()) {
-			throw new ArrayIndexOutOfBoundsException(writerIndex + " >= " + this.memory.capacity());
-		}
-		int old = this.writerIndex;
-		this.writerIndex = writerIndex;
-		return old;
-	}
+    /**
+     * @return the number of writable bytes
+     */
+    @Api public int writableBytes() {
+        return this.memory.capacity() - this.writerIndex;
+    }
 
-	/**
-	 * @return the number of readable bytes
-	 */
-	public int readableBytes() {
-		return this.writerIndex - this.readerIndex;
-	}
+    /**
+     * Resets the reader and writer index
+     */
+    @Api public void clear() {
+        this.readerIndex = 0;
+        this.writerIndex = 0;
+    }
 
-	/**
-	 * @return the number of writable bytes
-	 */
-	public int writableBytes() {
-		return this.memory.capacity() - this.writerIndex;
-	}
+    /**
+     * Increases the {@code readerIndex} by {@code length}<br>
+     * The same as {@link #increaseReaderIndex(int)}
+     *
+     * @return the old reader index
+     */
+    @Api public int skipBytes(int length) {
+        return readerIndex(readerIndex() + length);
+    }
 
-	/**
-	 * Resets the reader and writer index
-	 */
-	public void clear() {
-		this.readerIndex = 0;
-		this.writerIndex = 0;
-	}
+    /**
+     * Increases the {@code readerIndex} by {@code length}
+     *
+     * @return the old reader index
+     */
+    @Api public int increaseReaderIndex(int length) {
+        return readerIndex(readerIndex() + length);
+    }
 
-	/**
-	 * Increases the {@code readerIndex} by {@code length}<br>
-	 * The same as {@link #increaseReaderIndex(int)}
-	 * 
-	 * @param length
-	 * @return the old reader index
-	 */
-	public int skipBytes(int length) {
-		return readerIndex(readerIndex() + length);
-	}
+    /**
+     * Increases the {@code writerIndex} by {@code length}
+     *
+     * @return the old writer index
+     */
+    @Api public int increaseWriterIndex(int length) {
+        return writerIndex(writerIndex() + length);
+    }
 
-	/**
-	 * Increases the {@code readerIndex} by {@code length}
-	 * 
-	 * @param length
-	 * @return the old reader index
-	 */
-	public int increaseReaderIndex(int length) {
-		return readerIndex(readerIndex() + length);
-	}
+    @Api public void writeByte(byte value) {
+        memory.setByte(increaseWriterIndex(Byte.BYTES), value);
+    }
 
-	/**
-	 * Increases the {@code writerIndex} by {@code length}
-	 * 
-	 * @param length
-	 * @return the old writer index
-	 */
-	public int increaseWriterIndex(int length) {
-		return writerIndex(writerIndex() + length);
-	}
+    @Api public void writeShort(short value) {
+        memory.setShort(increaseWriterIndex(Short.BYTES), value);
+    }
 
-	/**
-	 * @param value
-	 */
-	public void writeByte(byte value) {
-		memory.setByte(increaseWriterIndex(Byte.BYTES), value);
-	}
+    @Api public void writeList(List<? extends BufferObject> list) {
+        writeInt(list.size());
+        for (BufferObject object : list) {
+            write(object);
+        }
+    }
 
-	/**
-	 * @param value
-	 */
-	public void writeShort(short value) {
-		memory.setShort(increaseWriterIndex(Short.BYTES), value);
-	}
+    @Api public <T extends BufferObject> void readList(List<T> list, Supplier<T> instanceCreator) {
+        list.clear();
+        int size = readInt();
+        for (int i = 0; i < size; i++) {
+            T o = instanceCreator.get();
+            read(o);
+            list.add(o);
+        }
+    }
 
-	/**
-	 * @param value
-	 */
-	public void writeInt(int value) {
-		memory.setInt(increaseWriterIndex(Integer.BYTES), value);
-	}
+    @Api public void writeInt(int value) {
+        memory.setInt(increaseWriterIndex(Integer.BYTES), value);
+    }
 
-	/**
-	 * @param value
-	 */
-	public void writeLong(long value) {
-		memory.setLong(increaseWriterIndex(Long.BYTES), value);
-	}
+    @Api public void writeLong(long value) {
+        memory.setLong(increaseWriterIndex(Long.BYTES), value);
+    }
 
-	/**
-	 * @param value
-	 */
-	public void writeFloat(float value) {
-		memory.setFloat(increaseWriterIndex(Float.BYTES), value);
-	}
+    @Api public void writeFloat(float value) {
+        memory.setFloat(increaseWriterIndex(Float.BYTES), value);
+    }
 
-	/**
-	 * @param value
-	 */
-	public void writeDouble(double value) {
-		memory.setDouble(increaseWriterIndex(Double.BYTES), value);
-	}
+    @Api public void writeDouble(double value) {
+        memory.setDouble(increaseWriterIndex(Double.BYTES), value);
+    }
 
-	/**
-	 * Writes the given byte array to this {@link PacketBuffer}
-	 * 
-	 * @param value
-	 */
-	public void writeBytes(byte[] value) {
-		writeInt(value.length);
-		writeBytes(value, 0, value.length);
-	}
+    /**
+     * Writes the given byte array to this {@link PacketBuffer}
+     */
+    @Api public void writeBytes(byte[] value) {
+        writeInt(value.length);
+        writeBytes(value, 0, value.length);
+    }
 
-	/**
-	 * Writes the given byte array to this {@link PacketBuffer} from offset
-	 * {@code offset} with length {@code length}
-	 * 
-	 * @param value
-	 * @param offset
-	 * @param length
-	 */
-	public void writeBytes(byte[] value, int offset, int length) {
-		memory.setBytes(increaseWriterIndex(length), value, offset, length);
-	}
+    /**
+     * Writes the given byte array to this {@link PacketBuffer} from offset
+     * {@code offset} with length {@code length}
+     */
+    @Api public void writeBytes(byte[] value, int offset, int length) {
+        memory.setBytes(increaseWriterIndex(length), value, offset, length);
+    }
 
-	/**
-	 * Writes a string to this {@link PacketBuffer}
-	 * 
-	 * @param string
-	 */
-	public void writeString(String string) {
-		byte[] data = string.getBytes(StandardCharsets.UTF_8);
-		writeBytes(data);
-	}
+    /**
+     * Writes a string to this {@link PacketBuffer}
+     */
+    @Api public void writeString(String string) {
+        byte[] data = string.getBytes(StandardCharsets.UTF_8);
+        writeBytes(data);
+    }
 
-	/**
-	 * Writes a {@link BufferObject} at the current {@link #writerIndex()}
-	 * 
-	 * @param object
-	 */
-	public void write(BufferObject object) {
-		object.write(this);
-	}
+    /**
+     * Writes a {@link BufferObject} at the current {@link #writerIndex()}
+     */
+    @Api public void write(BufferObject object) {
+        object.write(this);
+    }
 
-	/**
-	 * @return the byte at the current {@link #readerIndex()}
-	 */
-	public byte readByte() {
-		return memory.getByte(increaseReaderIndex(Byte.BYTES));
-	}
+    /**
+     * @return the byte at the current {@link #readerIndex()}
+     */
+    @Api public byte readByte() {
+        return memory.getByte(increaseReaderIndex(Byte.BYTES));
+    }
 
-	/**
-	 * @return the short at the current {@link #readerIndex()}
-	 */
-	public short readShort() {
-		return memory.getShort(increaseReaderIndex(Short.BYTES));
-	}
+    /**
+     * @return the short at the current {@link #readerIndex()}
+     */
+    @Api public short readShort() {
+        return memory.getShort(increaseReaderIndex(Short.BYTES));
+    }
 
-	/**
-	 * @return the int at the current {@link #readerIndex()}
-	 */
-	public int readInt() {
-		return memory.getInt(increaseReaderIndex(Integer.BYTES));
-	}
+    /**
+     * @return the int at the current {@link #readerIndex()}
+     */
+    @Api public int readInt() {
+        return memory.getInt(increaseReaderIndex(Integer.BYTES));
+    }
 
-	/**
-	 * @return the long at the current {@link #readerIndex()}
-	 */
-	public long readLong() {
-		return memory.getLong(increaseReaderIndex(Long.BYTES));
-	}
+    /**
+     * @return the long at the current {@link #readerIndex()}
+     */
+    @Api public long readLong() {
+        return memory.getLong(increaseReaderIndex(Long.BYTES));
+    }
 
-	/**
-	 * @return the float at the current {@link #readerIndex()}
-	 */
-	public float readFloat() {
-		return memory.getFloat(increaseReaderIndex(Float.BYTES));
-	}
+    /**
+     * @return the float at the current {@link #readerIndex()}
+     */
+    @Api public float readFloat() {
+        return memory.getFloat(increaseReaderIndex(Float.BYTES));
+    }
 
-	/**
-	 * @return the double at the current {@link #readerIndex()}
-	 */
-	public double readDouble() {
-		return memory.getDouble(increaseReaderIndex(Double.BYTES));
-	}
+    /**
+     * @return the double at the current {@link #readerIndex()}
+     */
+    @Api public double readDouble() {
+        return memory.getDouble(increaseReaderIndex(Double.BYTES));
+    }
 
-	/**
-	 * @return a read byte array from this {@link PacketBuffer}
-	 */
-	public byte[] readBytes() {
-		int length = readInt();
-		byte[] data = new byte[length];
-		readBytes(data, 0, length);
-		return data;
-	}
+    /**
+     * @return a read byte array from this {@link PacketBuffer}
+     */
+    @Api public byte[] readBytes() {
+        int length = readInt();
+        byte[] data = new byte[length];
+        readBytes(data, 0, length);
+        return data;
+    }
 
-	/**
-	 * Reads a string from this {@link PacketBuffer}
-	 * 
-	 * @return the read string
-	 */
-	public String readString() {
-		return new String(readBytes(), StandardCharsets.UTF_8);
-	}
+    /**
+     * Reads a string from this {@link PacketBuffer}
+     *
+     * @return the read string
+     */
+    @Api public String readString() {
+        return new String(readBytes(), StandardCharsets.UTF_8);
+    }
 
-	/**
-	 * Reads a {@link BufferObject} from the current {@link #readerIndex()}
-	 * 
-	 * @param object
-	 */
-	public void read(BufferObject object) {
-		object.read(this);
-	}
+    /**
+     * Reads a {@link BufferObject} from the current {@link #readerIndex()}
+     */
+    @Api public void read(BufferObject object) {
+        object.read(this);
+    }
 
-	/**
-	 * Reads {@code length} bytes from this {@link PacketBuffer} into the specified
-	 * byte array at position {@code offset}
-	 * 
-	 * @param value
-	 * @param offset
-	 * @param length
-	 */
-	public void readBytes(byte[] value, int offset, int length) {
-		memory.getBytes(increaseReaderIndex(length), value, offset, length);
-	}
+    /**
+     * Reads {@code length} bytes from this {@link PacketBuffer} into the specified
+     * byte array at position {@code offset}
+     */
+    @Api public void readBytes(byte[] value, int offset, int length) {
+        memory.getBytes(increaseReaderIndex(length), value, offset, length);
+    }
 
-	/**
-	 * @return the memory of this buffer
-	 */
-	public ByteMemory getMemory() {
-		return memory;
-	}
+    /**
+     * @return the memory of this buffer
+     */
+    @Api public ByteMemory getMemory() {
+        return memory;
+    }
 
-	/**
-	 * Sets the memory of this buffer
-	 * 
-	 * @param memory
-	 */
-	public void setMemory(ByteMemory memory) {
-		this.memory = memory;
-	}
+    /**
+     * Sets the memory of this buffer
+     */
+    @Api public void setMemory(ByteMemory memory) {
+        this.memory = memory;
+    }
 
 }
