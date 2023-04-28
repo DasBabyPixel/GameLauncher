08f1c46463efc55c4e480aac79efb30edf4d0f2d
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-04-28 09:50:06 +0200

    updated

--- base/src/main/java/gamelauncher/engine/network/packet/BufferObject.java
+++ base/src/main/java/gamelauncher/engine/network/packet/BufferObject.java
@@ -5,18 +5,14 @@ package gamelauncher.engine.network.packet;
  */
 public interface BufferObject {
 
-	/**
-	 * Writes this object to the buffer
-	 * 
-	 * @param buffer
-	 */
-	void write(PacketBuffer buffer);
+    /**
+     * Writes this object to the buffer
+     */
+    void write(PacketBuffer buffer);
 
-	/**
-	 * Reads data from the buffer into this object
-	 * 
-	 * @param buffer
-	 */
-	void read(PacketBuffer buffer);
+    /**
+     * Reads data from the buffer into this object
+     */
+    void read(PacketBuffer buffer);
 
 }
