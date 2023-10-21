3a85e2c466616cf727f7e40c51ba0ccf75bd4fe0
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-10-21 17:58:40 +0200

    fix access problems

--- lwjgl/src/main/java/module-info.java
+++ lwjgl/src/main/java/module-info.java
@@ -15,4 +15,14 @@ open module gamelauncher.lwjgl {
     requires org.lwjgl.stb.natives;
     requires de.dasbabypixel.property;
     requires java.desktop;
+    exports gamelauncher.lwjgl;
+    exports gamelauncher.lwjgl.gui;
+    exports gamelauncher.lwjgl.settings;
+    exports gamelauncher.lwjgl.input;
+    exports gamelauncher.lwjgl.event;
+    exports gamelauncher.lwjgl.util;
+    exports gamelauncher.lwjgl.util.image;
+    exports gamelauncher.lwjgl.util.keybind;
+    exports gamelauncher.lwjgl.render;
+    exports gamelauncher.lwjgl.render.glfw;
 }
